93e7fa1bb0fa95e7642efb21cad0d9431a86774a
Giant <me@liuju.cc>
2026-06-26 16:40:33 +0800

    部署脚本

--- Deploy.Live.ps1
+++ Deploy.Live.ps1
@@ -1,1 +1,7 @@
-Start-Deploy -ComputerName 10.88.19.3 -ComputerPort 5986 -UseSSL -WebSiteName WMSDoc24CTSIL004 -WebSitePort 80 -ScriptBlock { npm run docs:build } -OutputPath .\docs\.vitepress\dist\ -RemotePath "D:\Publish\"
+﻿$Credential = Get-Credential -UserName "Administrator" -Message "请输入部署服务器的用户名和密码"
+$SessionOption = New-PSSessionOption -SkipCACheck -SkipCNCheck -SkipRevocationCheck
+$Session = New-PSSession -ComputerName 10.88.19.3 -Port 5986 -Credential $Credential -UseSSL -SessionOption $SessionOption
+Start-DeployIIS -Session $Session -WebSiteName WMSDoc -WebSitePort 80 -ScriptBlock {
+    npm run docs:build
+} -OutputPath .\docs\.vitepress\dist\ -RemotePath "D:\Publish\"
+Remove-PSSession -Session $Session
--- Deploy.Test.ps1
+++ Deploy.Test.ps1
@@ -1,1 +1,7 @@
-Start-Deploy -ComputerName 10.76.99.18 -WebSiteName WMSDoc -WebSitePort 80 -ScriptBlock { npm run docs:build } -OutputPath .\docs\.vitepress\dist\ -RemotePath "D:\Publish\"
+﻿$Credential = Get-Credential -UserName "Administrator" -Message "请输入部署服务器的用户名和密码"
+$SessionOption = New-PSSessionOption -SkipCACheck -SkipCNCheck -SkipRevocationCheck
+$Session = New-PSSession -ComputerName 10.76.99.18 -Port 5985 -Credential $Credential -UseSSL:$false -SessionOption $SessionOption
+Start-DeployIIS -Session $Session -WebSiteName WMSDoc -WebSitePort 80 -ScriptBlock {
+    npm run docs:build
+} -OutputPath .\docs\.vitepress\dist\ -RemotePath "D:\Publish\"
+Remove-PSSession -Session $Session
